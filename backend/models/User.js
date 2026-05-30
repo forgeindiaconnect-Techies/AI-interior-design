764c8dde9fc9b868cfeb458176f753f7f7518d99
renugopal603-blip <renugopal603@gmail.com>
2026-05-30 10:13:32 +0530

    Fix User model pre-save hook for Mongoose 9 compatibility

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,11 +22,10 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Password hash middleware
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+UserSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Match password
